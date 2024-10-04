2b8dcc102f8d50317f1cf9e133c6203c0cc36e75
boiwantlearncode <ibetank@gmail.com>
2024-10-04 17:38:59 +0800

    updated @crease/test

--- packages/test/src/App.tsx
+++ packages/test/src/App.tsx
@@ -3,28 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Quiz } from '@crease/react'
-import type { Content, NonEmptyArray } from '@crease/react'
+import type { MixedContent, NonEmptyArray } from '@crease/react'
 
-const content: NonEmptyArray<Content> = [
+const content: NonEmptyArray<MixedContent> = [
   {
+    format: "multiple-answers",
     question: "How much wood could a woodchuck chuck if a woodchuck could chuck wood?",
     options: ["A", "B", "C", "D"],
-    correctAnswerIndex: 2
+    correctAnswers: [1, 2]
   },
   {
+    format: "single-answer",
     question: "What is the capital of France?",
     options: ["Paris", "London", "Berlin", "Madrid"],
-    correctAnswerIndex: 0
+    correctAnswers: 0
   },
   {
+    format: "single-answer",
     question: "What is the capital of Germany?",
     options: ["Paris", "London", "Berlin", "Madrid"],
-    correctAnswerIndex: 2
+    correctAnswers: 2
   },
   {
+    format: "single-answer",
     question: "What is the capital of Spain?",
     options: ["Paris", "London", "Berlin", "Madrid"],
-    correctAnswerIndex: 3
+    correctAnswers: 3
   }
 ]
 
@@ -33,7 +37,7 @@ function App() {
 
   return (
     <>
-      <Quiz theme={'light'} shuffle={false} format='single-answer' content={content} />
+      <Quiz theme={'light'} shuffle={false} content={content} />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
